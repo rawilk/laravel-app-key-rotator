63bec6a1d4d08fd64e479b0bb9b1bed84b15e8ab
Randall Wilk <wilk.randall@gmail.com>
2022-11-07 14:49:10 -0600

    Fix typo

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 composer install
-cp composer.lock vendor/orchestra/testbench-core/compser.lock
+cp composer.lock vendor/orchestra/testbench-core/laravel/composer.lock
 php ./vendor/bin/testbench package:discover --ansi
